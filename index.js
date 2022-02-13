2899b57cc6bcecc969d170adb75cf6cfa039b1eb
brian-lets-go <brian.w.meenan@gmail.com>
2022-02-13 16:57:51 -0500

    testing inquire

---
+++ index.js
@@ -0,0 +1,62 @@
+// Packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('generateMarkdown.js');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = [
+
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter your project title');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub Username?',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your email address!');
+                return false;
+            }
+        }
+    },
+
+];
+
+inquirer.prompt(questions);
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
