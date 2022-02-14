3c6fcbdd9965d97e6ee8d4a91fe1fdca991fce46
brian-lets-go <brian.w.meenan@gmail.com>
2022-02-13 21:57:08 -0500

    questions added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const generateMarkdown = require('./generateMarkdown.js');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [
+const questionArray = [
 
     {
         type: 'input',
@@ -42,21 +42,159 @@ const questions = [
             if (githubInput) {
                 return true;
             } else {
-                console.log('Please enter your email address!');
+                console.log('Please enter your email address');
                 return false;
             }
         }
     },
 
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe you project',
+        validate: whatInput => {
+            if (whatInput) {
+                return true;
+            } else {
+                console.log('Please enter a description for your project');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide step-by-step installation instructions for your project',
+        validate: installInput => {
+            if (installInput) {
+                return true;
+            } else {
+                console.log('Please enter your installation instructions!');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'instructions',
+        message: 'Please provide instructions to use your project',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please enter your use instructions');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license will you use for your project?',
+        choices: ['agpl', 'apache', 'mit', 'no license']
+    },
+
+    {
+        type: 'confirm',
+        name: 'contributers',
+        message: 'Would you like to allow other contributors?',
+        default: true
+    },
+
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'Please provide contribution instructions',
+        when: ({ contributers }) => {
+            if (contributers) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: contributerInput => {
+            if (contributerInput) {
+                return true;
+            } else {
+                console.log('Please enter contribution instructions');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please provide instructions on how to test the app',
+        validate: testInput => {
+            if (testInput) {
+                return true;
+            } else {
+                console.log('Please enter your test instructions');
+                return false;
+            }
+        }
+    }
+
 ];
 
-inquirer.prompt(questions);
+// inquirer.prompt(questions);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+const writeToFile = htmlData => {
 
-// TODO: Create a function to initialize app
-function init() {}
+    return new Promise((resolve, reject) => {
+
+        fs.writeFile('./generated-README.md', htmlData, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'README File Createed'
+            });
+        });
+    });
+}
+
+// Function to initialize app
+const init = () => {
+
+    return inquirer.prompt(questionArray)
+    .then(answers => {
+        return answers;
+    })
+
+}
 
 // Function call to initialize app
-init();
+init()
+
+.then(answers => {
+
+    console.log(answers);
+    generateMarkdown(answers);
+})
+
+.then(pageMD => {
+
+    writeFile(pageMD);
+
+})
+
+.then(writeFileResponse => {
+
+    console.log(writeFileResponse.message);
+
+})
+
+.catch(err => {
+
+    console.log(err);
+
+})
